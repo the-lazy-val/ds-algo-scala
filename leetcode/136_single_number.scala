efc0ae55c7034c7d914036e84ec5d792d90b29f2
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-04 08:45:07 +0530

    Update 136_single_number.scala

--- leetcode/136_single_number.scala
+++ leetcode/136_single_number.scala
@@ -14,6 +14,16 @@ object Solution {
 }
 
 //Alternate solution (not much time improvement)
+/**
+scala> 3 ^ 3
+res0: Int = 0
+
+scala> 3 ^ 4
+res1: Int = 7
+
+scala> 3 ^ 4 ^ 3
+res2: Int = 4
+*/
 object Solution {
     def singleNumber(nums: Array[Int]): Int = {
         nums.reduce(_ ^ _)
