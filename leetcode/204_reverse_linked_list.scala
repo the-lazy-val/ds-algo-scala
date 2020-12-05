3151bdd8064383ed05986595cbc9a4778b9d90b0
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-05 08:26:43 +0530

    Create 204_reverse_linked_list.scala

---
+++ leetcode/204_reverse_linked_list.scala
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(_x: Int = 0, _next: ListNode = null) {
+ *   var next: ListNode = _next
+ *   var x: Int = _x
+ * }
+ */
+object Solution {
+    def reverseList(head: ListNode): ListNode = {
+        if(head == null || head.next == null){
+            head
+        } else{
+            var p = reverseList(head.next)
+            head.next.next = head
+            head.next = null
+            p
+        }
+    }
+}
