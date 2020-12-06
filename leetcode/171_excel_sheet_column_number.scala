2907702c4bc69fdec55c3482365fd6d4e25bc725
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-06 10:58:56 +0530

    Create 171_excel_sheet_column_number.scala

---
+++ leetcode/171_excel_sheet_column_number.scala
@@ -0,0 +1,14 @@
+object Solution {
+    def titleToNumber(s: String): Int = {
+        var size = s.size
+        var i = 0
+        var sheetNumber = 0
+        while(i < size){
+            var sum = (s.charAt(i) - 'A' + 1) * Math.pow(26, (size - i-1)).toInt
+            
+            sheetNumber += sum
+            i += 1
+        }
+        sheetNumber
+    }
+}
