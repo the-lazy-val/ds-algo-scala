e9cca871a554931efde0ae68a32e81253fe12954
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-07 17:29:25 +0530

    Update 387_first_unique_char.scala

--- leetcode/387_first_unique_char.scala
+++ leetcode/387_first_unique_char.scala
@@ -19,3 +19,20 @@ object Solution {
         if(uniqChar.nonEmpty) uniqChar.get._2 else -1 
     }
 }
+
+/**How the above solution works:
+scala> val a = "akashs"
+a: String = akashs
+
+scala> a.toCharArray.groupBy(identity)
+res0: scala.collection.immutable.Map[Char,Array[Char]] = Map(h -> Array(h), k -> Array(k), s -> Array(s, s), a -> Array(a, a))
+
+scala> a.toCharArray.groupBy(identity).mapValues(_.length)
+res1: scala.collection.immutable.Map[Char,Int] = Map(h -> 1, k -> 1, s -> 2, a -> 2)
+
+scala> a.toCharArray.zipWithIndex
+res2: Array[(Char, Int)] = Array((a,0), (k,1), (a,2), (s,3), (h,4), (s,5))
+
+scala> a.toCharArray.zipWithIndex.find(x => res1(x._1) == 1)
+res3: Option[(Char, Int)] = Some((k,1))
+*/
