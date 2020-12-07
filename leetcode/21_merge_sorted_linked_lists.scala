1f71185dd00b65ebe53f18cf467bffdcc843d700
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-07 08:00:06 +0530

    Update 21_merge_sorted_linked_lists.scala

--- leetcode/21_merge_sorted_linked_lists.scala
+++ leetcode/21_merge_sorted_linked_lists.scala
@@ -40,3 +40,27 @@ object Solution {
         }
     }
 }
+
+
+//Alternate & Simpler solution
+/**
+ * Definition for singly-linked list.
+ * class ListNode(_x: Int = 0, _next: ListNode = null) {
+ *   var next: ListNode = _next
+ *   var x: Int = _x
+ * }
+ */
+object Solution {
+    def mergeTwoLists(l1: ListNode, l2: ListNode): ListNode = {
+    if(l1 == null) return l2
+    if(l2 == null) return l1
+
+    if (l1.x < l2.x) {
+      l1.next = mergeTwoLists(l1.next, l2)
+      l1
+    } else {
+      l2.next = mergeTwoLists(l1, l2.next)
+      l2
+    }
+  }
+}
