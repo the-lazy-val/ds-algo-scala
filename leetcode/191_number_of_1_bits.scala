d94ec949394472a0fb8914df1cf80c05aa10f697
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-08 08:26:03 +0530

    Update 191_number_of_1_bits.scala

--- leetcode/191_number_of_1_bits.scala
+++ leetcode/191_number_of_1_bits.scala
@@ -4,3 +4,27 @@ object Solution {
         n.toBinaryString.toCharArray.filter(_ == '1').length
     }
 }
+
+//Alternate bit-wise shift and count 1
+def hammingWeight(n: Int): Int = {
+        var count = 0
+        var num   = n
+
+        while (num != 0) {
+            if ((num & 1) > 0) { count = count + 1 }
+            num = num >>> 1
+        }
+    count
+}
+
+//Alternate (need to understand whats happening here) AND of num, num-1
+def hammingWeight(n: Int): Int = {
+        println(n)
+        var sum = 0
+        var num = n         
+        while (num != 0) {
+            sum += 1
+            num &= (num-1)                
+        }
+        sum
+    }
