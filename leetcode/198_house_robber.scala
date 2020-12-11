39614cf12f771c9f33245f241a3e1eafa03442bd
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-11 10:15:34 +0530

    Update 198_house_robber.scala

--- leetcode/198_house_robber.scala
+++ leetcode/198_house_robber.scala
@@ -19,3 +19,20 @@ object Solution {
 //but had MEMORY LIMIT EXCEEDED for large input
 
 //While using DP: we try to store values of repetitive calculations
+object Solution {
+    def rob(nums: Array[Int]): Int = {
+        if(nums.length == 0){
+            0
+        }else{
+            var dp = Array.fill(nums.length+1)(0)
+            
+            dp(0) = 0
+            dp(1) = nums(0)
+            (1 to nums.length-1).map(i => {
+                dp(i+1) = Math.max(dp(i), dp(i-1) + nums(i))
+            })
+            
+            dp(nums.length)
+        }
+    }
+}
