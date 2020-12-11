a53c23e7621862e905064afe73b0d65874a8e47a
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-11 07:41:19 +0530

    Update 155_min_stack.scala

--- leetcode/155_min_stack.scala
+++ leetcode/155_min_stack.scala
@@ -28,3 +28,36 @@ class MinStack() {
     }
 
 }
+
+//Alternate solution: much faster
+//Here we are prepending elements to the list instead of appending
+//Note that since List is actually a LinkedList its much easier to deal with "head" of the list
+//There is also another list to maintain min elements of the list
+class MinStack() {
+
+    /** initialize your data structure here. */
+    var stack = List.empty[Int]
+    var mins = List.empty[Int]
+
+    def push(x: Int) {
+        //this line fails if we make second condition as x < mins.head
+        //with NoSuchElementException: head of empty list
+        //why???
+        if(mins.isEmpty || mins.head >= x) mins = x +: mins
+        stack = x +: stack
+    }
+
+    def pop() {
+        if(mins.head == stack.head) mins = mins.tail
+        stack = stack.tail
+    }
+
+    def top(): Int = {
+        stack.head
+    }
+
+    def getMin(): Int = {
+        mins.head
+    }
+
+}
