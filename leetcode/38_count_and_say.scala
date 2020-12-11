663a8cafaf30a33d27970e5aebaef71b0391266a
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-11 08:06:54 +0530

    Update 38_count_and_say.scala

--- leetcode/38_count_and_say.scala
+++ leetcode/38_count_and_say.scala
@@ -27,3 +27,38 @@ object Solution {
         }
     }
 }
+
+
+//Just using StringBuilder put the solution from 16% -> 93.5% in terms of time efficiency
+
+object Solution {
+    def countAndSay(n: Int): String = {
+        if(n == 1){
+            "1"
+        }else{
+            val prev = countAndSay(n-1)
+            val prevIntArray = prev.toCharArray.map(x => x - '0')
+            
+            var output = new scala.collection.mutable.StringBuilder()
+            var count = 0
+            if(!prevIntArray.isEmpty){
+                var element = prevIntArray(0)
+            
+                for(elem <- prevIntArray){
+                    if(elem == element){
+                        count += 1
+                    }else{
+                        output.append(count)
+                        output.append(element)
+                        element = elem
+                        count = 1
+                    }
+                }
+                output.append(count)
+                output.append(element)
+            }
+            
+            output.toString
+        }
+    }
+}
