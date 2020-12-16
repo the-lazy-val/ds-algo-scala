d2e4c7f3dc6ec36b92f7a7ec1c8ed18ce167ae86
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-16 08:15:40 +0530

    Update 160_intersection_linked_list.scala

--- leetcode/160_intersection_linked_list.scala
+++ leetcode/160_intersection_linked_list.scala
@@ -9,6 +9,75 @@
 object Solution {
     
     def getIntersectionNode(headA: ListNode, headB: ListNode): ListNode = {
+        if(headA == null){
+            null
+        }else if(headB == null){
+            null
+        }else{
+            var ha = headA
+        var hb = headB
+        
+        var hAsize = 0
+        var hBsize = 0
+        while(ha.next != null){
+            hAsize += 1
+            ha = ha.next
+        }
+        while(hb.next != null){
+            hBsize += 1
+            hb = hb.next
+        }
+        
+        var first: ListNode = null
+        var second: ListNode = null
+        var diff = 0
+        
+        if(hAsize>hBsize){
+            first = headA
+            second = headB
+            diff=hAsize-hBsize
+        }else{
+            first = headB
+            second = headA
+            diff=hBsize-hAsize
+        }
+        
+        while(diff > 0){
+            first = first.next
+            diff -= 1
+        }
+        
+        var result: ListNode = null
+        import scala.util.control.Breaks._
+        breakable{
+        while(first!= null && second!= null){
+            if(first == second){
+                result = first
+                break
+            }
+            first = first.next
+            second = second.next
+        }
+        }
+        
+        result
+        }
+    }
+}
+
+//Alternate solution
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var _x: Int = 0) {
+ *   var next: ListNode = null
+ *   var x: Int = _x
+ * }
+ */
+
+object Solution {
+    
+    def getIntersectionNode(headA: ListNode, headB: ListNode): ListNode = {
         var ha = headA
         var hb = headB
         
