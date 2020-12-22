47a59f5b5964d1177bfc7ae1296c8c2b70b4713b
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-22 10:07:37 +0530

    Create 46_permutations.scala

---
+++ leetcode/46_permutations.scala
@@ -0,0 +1,28 @@
+object Solution {
+    var output = List.empty[List[Int]]
+    
+    def backtrack(nums: Array[Int], l: Int, r: Int): Unit = {
+        def swap(a: Int, b: Int) = {
+            val temp = nums(a)
+            nums(a) = nums(b)
+            nums(b) = temp
+        }
+        
+        if(l == r){
+            output = output :+ nums.toList
+        }else{
+            (l to r).map(i => {
+                swap(l, i)
+                backtrack(nums, l+1, r)
+                swap(l, i) //backtrack step
+            })
+        }
+    }
+    
+    def permute(nums: Array[Int]): List[List[Int]] = {
+        output = List.empty[List[Int]]
+        var input = nums
+        backtrack(input, 0, input.length - 1)
+        output
+    }
+}
