f226095582c8e2f00374aadc47e27aef727019b7
the-lazy-val <46882662+the-lazy-val@users.noreply.github.com>
2020-12-29 10:36:34 +0530

    Update 347_top_k_frequent_elements.scala

--- leetcode/347_top_k_frequent_elements.scala
+++ leetcode/347_top_k_frequent_elements.scala
@@ -7,6 +7,22 @@ object Solution {
                 case None => hm += (elem -> 1)
             }
         }
-        hm.toList.sortBy(x => (x._2)*(-1)).take(k).map(_._1).toArray
+        
+        // hm.toList.sortBy(x => (x._2)*(-1)).take(k).map(_._1).toArray
+        // The above is a sorting approach. We can use Heap/PriorityQueue to achieve better time complexity
+        
+        import scala.math.Ordering.Implicits._
+        
+        def orderByFrequency(tup: (Int, Int)) = tup._2
+        
+        val pq = new scala.collection.mutable.PriorityQueue[(Int, Int)]()(Ordering.by(orderByFrequency))
+        
+        for(entry <- hm){
+            pq.enqueue(entry)
+        }
+        
+        println(pq)
+        (1 to k).map(_ => pq.dequeue).map(_._1).toArray
+        
     }
 }
